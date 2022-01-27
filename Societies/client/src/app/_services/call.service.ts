b6f6e818d1e3c075c49b327de780aca72d704497
Yewo Theu <yewotheu123456789@gmail.com>
2022-01-27 13:20:14 +0200

    Created Registration Page
    Created the deisgn for the register page

---
+++ Societies/client/src/app/_services/call.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CallService {
+
+  constructor() { }
+
+  call(num: string){
+    window.open('tel:' + num);
+  }
+}
